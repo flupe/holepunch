f6ad4954cac08de7c8cdf4811bad4d190cee6c69
flupe <lucas@escot.me>
2026-01-17 22:17:57 +0100

    basic aseprite loading

--- main.js
+++ main.js
@@ -1,29 +1,15 @@
 const on = (t, e, f) => t.addEventListener(e, f, false)
+const hex = (c) => 0xff000000 | c.b << 16 | c.g << 8 | c.r
 
-let activePoll = null
-
-function pollLastModified(file, date) {
-  return window.setInterval(() => {
-    console.log(filepick.files[0])
-  }, 1000)
-}
+const palette = []
 
 async function processFile() {
-  if (activePoll !== null)
-    window.clearInterval(activePoll)
-
   // exit if less/more than 1 file is selected
   if (filepick.files.length != 1) return
 
   const file   = filepick.files[0]
   const buffer = await file.arrayBuffer()
   const view   = new DataView(buffer)
-
-
-
-  console.log(file)
-
-  pollLastModified(file, file.lastModified)
 
   let offset = 0
 
@@ -44,7 +30,7 @@ async function processFile() {
     return v
   }
 
-  console.log(`Filesize: ${u32()}`)
+  const size  = u32()
   const magic = u16()
 
   if (magic != 0xa5e0)
@@ -55,8 +41,116 @@ async function processFile() {
   const height     = u16()
   const colorDepth = u16()
 
-  console.log(`Canvas size: ${width}x${height}`)
+  const data = new Uint8Array(width * height)
 
+  // indexed mode to use palettes
+  if (colorDepth != 8)
+    throw new Error(`Unsupported color depth: ${colorDepth}. Aseprite file should be in indexed mode.`)
+
+  offset += 20
+
+  const pwidth  = u8() || 1
+  const pheight = u8() || 1
+
+  const ratio = pwidth / pheight
+
+  offset = 128 // skip rest of header
+
+  // parse first frame
+
+  const frameBytes    = u32()
+  const frameMagic    = u16() // 0xf1fa
+  const chunkCount    = u16()
+  const frameDuration = u16()
+
+  offset += 6 // reserved space
+
+  if (chunkCount === 0xffff)
+      chunkCount = u32()
+
+  offset = 144
+
+  for (let c = 0; c < chunkCount; c++) {
+    const chunkStart = offset
+    const chunkSize  = u32()
+    const chunkType  = u16()
+
+    // not a CEL chunk, we skip it entirely
+    if (chunkType == 0x2019) {
+      console.log("(New) palette chunk: ignored")
+      throw new Error(`Found new palette chunk: not supported`)
+      offset = chunkStart + chunkSize
+      continue
+    }
+    else if (chunkType == 0x0004) {
+      numPackets = u16()
+
+      for (let p = 0; p < numPackets; p++) {
+        skipEntries = u8()
+        numColors   = u8() || 0xff
+
+        for (let col = 0; col < numColors; col++)
+          palette.push({r: u8(), g: u8(), b: u8()})
+      }
+
+      offset = chunkStart + chunkSize
+      continue
+    }
+    else if (chunkType == 0x0011) {
+      throw new Error(`Found old palette chunk: not supported`)
+      offset = chunkStart + chunkSize
+      continue
+    }
+    else if (chunkType != 0x2005) {
+      console.log("Skipping chunk")
+      offset = chunkStart + chunkSize
+      continue
+    }
+
+    const layerIndex = u16()
+    const x          = i16()
+    const y          = i16()
+    const opacity    = u8()
+    const celType    = u16()
+
+    offset += 7
+
+    if (celType === 0) {
+      console.log("raw image")
+      throw new Error(`Found raw image: not supported (yet)`)
+    }
+
+    else if (celType === 2) {
+      let cwidth  = u16()
+      let cheight = u16()
+      let rawData = buffer.slice(offset, chunkStart + chunkSize)
+      pako.inflate(rawData).forEach((v, i) => {
+        let px = x + (i % cwidth)
+        let py = y + Math.floor(i / cwidth)
+        console.log(v)
+        data[px + py * width] = v % palette.length
+      }) 
+    }
+
+    offset = chunkStart + chunkSize
+  }
+
+  // draw on canvas
+  canvas.width  = width
+  canvas.height = height
+
+  canvas.style.width  = `${3 * width * ratio}px`
+  canvas.style.height = `${3 * height}px`
+
+  let ctx = canvas.getContext('2d')
+  let buf = ctx.createImageData(width, height)
+  let img = new Uint32Array(buf.data.buffer)
+
+  data.forEach((x, i) => {
+    img[i] = hex(palette[data[i]])
+  })
+
+  ctx.putImageData(buf, 0, 0)
 }
 
 on(filepick, "change", processFile)
